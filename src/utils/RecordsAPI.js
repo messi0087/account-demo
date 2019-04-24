e6f36b6691745039a18c016eb50da18c24d19fa9
my-name <458657425@qq.com>
2019-04-24 16:03:21 +0800

    change

--- src/utils/RecordsAPI.js
+++ src/utils/RecordsAPI.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-export const api =process.env.REACT_APP_RECORDS_API_URL || "http://localhost:5000" ;
+export const api =process.env.REACT_APP_RECORDS_API_URL || "http://localhost:3004" ;
 
 export const getALL =() =>
     axios.get(`${api}/records`);
